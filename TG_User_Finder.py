c3f4b33f47b98456e97770d20423e8603b8996fc
skylersmith773 <149861467+skylersmith773@users.noreply.github.com>
2023-11-04 02:33:49 -0400

    Update TG_User_Finder.py

--- TG_User_Finder.py
+++ TG_User_Finder.py
@@ -37,8 +37,8 @@ def list_checker():
             print ("Null")
 
 if __name__ == '__main__':
-    phone = 'Your TG Number'
-    client = TelegramClient(phone, API ID, 'API Hash')
+    phone = '4015756255'
+    client = TelegramClient(phone, 29506233, 'c1126215187eb7311ac825fcdf7e7b62')
     client.connect()
     if not client.is_user_authorized():
         client.send_code_request(phone)
